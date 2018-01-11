c828897ad155f772e9187bcf7fe46fc81a60f606
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-11 17:49:55 +0100

    Add FunctionDescription

---
+++ Speccer/Description/FunctionDescription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Speccer.Description
+{
+    public class FunctionDescription
+    {
+        public string Name { get; }
+        public Type ReturnType { get; }
+        public IEnumerable<Type> Arguments { get; }
+
+        public FunctionDescription(string name, Type returnType, IEnumerable<Type> arguments)
+        {
+            Name = name;
+            ReturnType = returnType;
+            Arguments = arguments.ToList();
+        }
+    }
+}
