ed82065cc54c564381b8182d0165c130f0dd2a59
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-11 22:25:56 +0100

    Add NamespaceAnalyzer

---
+++ Speccer/Analysis/NamespaceAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Speccer.Analysis
+{
+    public static class NamespaceAnalyzer
+    {
+        public static NameSyntax ExtractFixtureNamespace(SyntaxTree tree)
+        {
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var namespaceDeclaration = root.DescendantNodes()
+                .OfType<NamespaceDeclarationSyntax>()
+                .First();
+            return namespaceDeclaration.Name;
+        }
+
+        public static string ToModuleNamespace(this NameSyntax fixtureNamespace)
+        {
+            return fixtureNamespace.ToString().Replace(".Test", "");
+        }
+    }
+}
