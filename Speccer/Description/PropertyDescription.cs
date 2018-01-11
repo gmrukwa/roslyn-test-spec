5543e2d54cf7881f96a9e9964e49ba9e34cbebbe
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-11 17:49:42 +0100

    Add PropertyDescription

---
+++ Speccer/Description/PropertyDescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Speccer.Description
+{
+    public class PropertyDescription
+    {
+        public string Name { get; }
+        public Type Type { get; }
+        public bool HasSetter { get; }
+
+        public PropertyDescription(string name, Type type, bool hasSetter)
+        {
+            Name = name;
+            Type = type;
+            HasSetter = hasSetter;
+        }
+    }
+}
