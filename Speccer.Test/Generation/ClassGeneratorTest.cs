987987dd3345706e279981fd480c64496c8d825d
Daroslaw <daroslawowicz@gmail.com>
2018-01-11 22:46:44 +0100

    Added class generator.

---
+++ Speccer.Test/Generation/ClassGeneratorTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Speccer.Description;
+using Speccer.Generation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Speccer.Test.Generation
+{
+    [TestClass]
+    public class ClassGeneratorTest
+    {
+        private ClassGenerator _classGenerator;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            string namespaceStr = "Namespace.SubNamespace";
+            string nameStr = "ClassName";
+            List<PropertyDescription> properties = new List<PropertyDescription>
+            {
+                new PropertyDescription("Property1", typeof(long), false),
+                new PropertyDescription("Property2", typeof(string), true),
+                new PropertyDescription("Property3", typeof(bool), true)
+            };
+
+            List<FunctionDescription> functions = new List<FunctionDescription>
+            {
+                new FunctionDescription("Method1", typeof(Object), new List<Type>()),
+                new FunctionDescription("Method2", typeof(void), new List<Type>
+                {
+                    typeof(string),
+                    typeof(int),
+                    typeof(bool)
+                }),
+            };
+
+            ClassDescription cd = new ClassDescription(
+                nameStr,
+                namespaceStr,
+                properties,
+                functions);
+
+            _classGenerator = new ClassGenerator(cd);
+        }
+
+        [TestMethod]
+        public void works()
+        {
+            string result = _classGenerator.GenerateClass();
+            Console.WriteLine(result);
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ Speccer/Generation/ClassGenerator.cs
@@ -0,0 +1,117 @@
+﻿using Speccer.Description;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Speccer.Generation
+{
+    public class ClassGenerator
+    {
+        private ClassDescription _classDescription;
+        private int _tabsAmount;
+
+        public ClassGenerator(ClassDescription classDescription)
+        {
+            _classDescription = classDescription;
+        }
+
+        public string GenerateClass()
+        {
+            StringBuilder sb = new StringBuilder();
+            _tabsAmount = 0;
+
+            BeginNamespace(sb, _classDescription.Namespace);
+            BeginClass(sb, _classDescription.Name);
+
+            foreach (var property in _classDescription.Properties)
+                AppendProperty(sb, property);
+
+            AppendLine(sb, "");
+
+            foreach (var method in _classDescription.Functions)
+                AppendMethod(sb, method);
+
+            CloseBracket(sb);
+            CloseBracket(sb);
+
+            return sb.ToString();
+        }
+
+        private string TextWithTabs(string text)
+        {
+            string result = "";
+            for (int i = 0; i < _tabsAmount; ++i)
+                result += "\t";
+            return result + text;
+        }
+
+        private void AppendLine(StringBuilder strBuilder, string line)
+        {
+            strBuilder.AppendLine(TextWithTabs(line));
+        }
+
+        private void OpenBracket(StringBuilder strBuilder)
+        {
+            AppendLine(strBuilder, "{");
+            _tabsAmount++;
+        }
+
+        private void CloseBracket(StringBuilder strBuilder)
+        {
+            _tabsAmount--;
+            AppendLine(strBuilder, "}");
+        }
+
+        private void BeginNamespace(StringBuilder strBuilder, string namespaceString)
+        {
+            AppendLine(strBuilder, $"namespace {namespaceString}");
+            OpenBracket(strBuilder);
+        }
+
+        private void BeginClass(StringBuilder strBuilder, string classString)
+        {
+            AppendLine(strBuilder, $"public class {classString}");
+            OpenBracket(strBuilder);
+        }
+
+        private void AppendProperty(StringBuilder strBuilder, PropertyDescription propertyDescription)
+        {
+            string propertyStr = "public ";
+            propertyStr += propertyDescription.Type.Name + " ";
+            propertyStr += propertyDescription.Name + " ";
+            propertyStr += "{ get; ";
+            if (propertyDescription.HasSetter)
+                propertyStr += "set; ";
+            propertyStr += "}";
+
+            AppendLine(strBuilder, propertyStr);
+        }
+
+        private void AppendMethod(StringBuilder strBuilder, FunctionDescription functionDescription)
+        {
+            string methodStr = "public ";
+            methodStr += functionDescription.ReturnType.Name + " ";
+            methodStr += functionDescription.Name + "(";
+            if(functionDescription.Arguments.ToList().Count != 0)
+            {
+                int argNum = 0;
+                foreach (var argType in functionDescription.Arguments)
+                {
+                    methodStr += argType.Name + $" arg{argNum}, ";
+                    argNum++;
+                }
+                int lastCommaIdx = methodStr.LastIndexOf(",");
+                if (lastCommaIdx >= 0)
+                    methodStr = methodStr.Substring(0, lastCommaIdx);
+            }
+            
+            methodStr += ")";
+
+            AppendLine(strBuilder, methodStr);
+            OpenBracket(strBuilder);
+            AppendLine(strBuilder, "throw new NotImplementedException();");
+            CloseBracket(strBuilder);
+        }
+    }
+}
