62158f532b856deb5fa5b7b2ec46e3928f9dc99c
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-11 18:51:51 +0100

    Add sample test file

---
+++ SampleTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sample.Namespace;
+
+namespace Sample.Test.Namespace
+{
+    [TestClass]
+    public class SampleTest
+    {
+        private Sample _sample;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _sample = new Sample();
+        }
+
+        [TestMethod]
+        public void throws_not_implemented()
+        {
+            Assert.ThrowsException<NotImplementedException>(
+                () => _sample.DoSmth("blah"));
+        }
+
+        [TestMethod]
+        public void does_smth_else()
+        {
+            _sample.Blah = "blah";
+            int result = _sample.DoSmthElse(_sample.Wololo);
+        }
+
+        [TestMethod]
+        public void does_smth_else_without_explicit_type()
+        {
+            var result = _sample.DoSmthElse(_sample.Wololo);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
