2cbe15b7ccb7385a8d90a69a1c094676aa2bad0e
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-11 17:21:41 +0100

    Add FixtureAnalyzer

---
+++ Speccer.Test/Analysis/FixtureAnalyzerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Speccer.Analysis;
+
+namespace Speccer.Test.Analysis
+{
+    [TestClass]
+    public class FixtureAnalyzerTest
+    {
+        private FixtureAnalyzer _analyzer;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _analyzer = new FixtureAnalyzer();
+        }
+
+        [TestMethod]
+        public void throws_not_implemented()
+        {
+            Assert.ThrowsException<NotImplementedException>(
+                () => _analyzer.ExtractSpecification("blah"));
+        }
+    }
+}
---
+++ Speccer/Analysis/FixtureAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Speccer.Description;
+
+namespace Speccer.Analysis
+{
+    public class FixtureAnalyzer
+    {
+        public ClassDescription ExtractSpecification(string testFixture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
