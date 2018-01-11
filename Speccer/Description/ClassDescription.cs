60397cd5720465e24488a3da39b0147bac9d9ecc
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-11 17:50:08 +0100

    Add ClassDescription

---
+++ Speccer/Description/ClassDescription.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Speccer.Description
+{
+    public class ClassDescription
+    {
+        public string Name { get; }
+        public string Namespace { get; }
+        public IEnumerable<PropertyDescription> Properties { get; }
+        public IEnumerable<FunctionDescription> Functions { get; }
+
+        public ClassDescription(string name, string namesp, IEnumerable<PropertyDescription> properties,
+            IEnumerable<FunctionDescription> functions)
+        {
+            Name = name;
+            Namespace = namesp;
+            Properties = properties.ToList();
+            Functions = functions.ToList();
+        }
+    }
+}
