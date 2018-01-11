e6005bbaf4d98c5473d6b5274cd1be2bf7cfd740
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-11 22:21:46 +0100

    Add FixtureNameAnalyzer

---
+++ Speccer/Analysis/FixtureNameAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Speccer.Analysis
+{
+    public static class FixtureNameAnalyzer
+    {
+        public static string ExtractFixtureName(SyntaxTree tree)
+        {
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var fixture = root.DescendantNodes()
+                .OfType<ClassDeclarationSyntax>()
+                .First();
+            var identifier = fixture
+                .ChildTokens()
+                .First(token => token.IsKind(SyntaxKind.IdentifierToken));
+            return (string)identifier.Value;
+        }
+
+        public static string ToTargetClassName(this string fixtureNamespace)
+        {
+            return fixtureNamespace.Replace("Test", "");
+        }
+    }
+}
