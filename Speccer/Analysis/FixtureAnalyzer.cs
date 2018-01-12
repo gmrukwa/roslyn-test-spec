1cefe156b75c321f6c190c0d62ca250f6e088700
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-12 19:33:49 +0100

    Generate class stub before compilation

--- Speccer/Analysis/FixtureAnalyzer.cs
+++ Speccer/Analysis/FixtureAnalyzer.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Speccer.Description;
+using Speccer.Generation;
 
 namespace Speccer.Analysis
 {
@@ -14,12 +17,7 @@ namespace Speccer.Analysis
         public ClassDescription ExtractSpecification(string testFixture)
         {
             var tree = CSharpSyntaxTree.ParseText(testFixture);
-            var compilation = CSharpCompilation.Create("TestedAssembly")
-                .AddReferences(
-                    MetadataReference.CreateFromFile(
-                        typeof(object).Assembly.Location))
-                .AddSyntaxTrees(tree);
-            var model = compilation.GetSemanticModel(tree);
+
 
             var namespaceName = NamespaceAnalyzer
                 .ExtractFixtureNamespace(tree)
@@ -28,11 +26,31 @@ namespace Speccer.Analysis
                 .ExtractFixtureName(tree)
                 .ToTargetClassName();
 
-            var specification = new ClassDescription(name: className,
-                namesp: namespaceName, properties: new PropertyDescription[]{},
-                functions: new FunctionDescription[]{});
 
-            return specification;
+            var emptyClassStub = buildTemporaryStub(namespaceName, className);
+            var stubTree = CSharpSyntaxTree.ParseText(emptyClassStub);
+
+            var compilation = CSharpCompilation.Create("TestedAssembly")
+                .AddReferences(
+                    MetadataReference.CreateFromFile(
+                        typeof(object).Assembly.Location))
+                .AddSyntaxTrees(stubTree)
+                .AddSyntaxTrees(tree);
+
+            var diagnostics = compilation.GetDiagnostics();
+            
+            var functions = tree.GetFunctionCallsOn(compilation, className);
+
+            return new ClassDescription(name: className,
+                namesp: namespaceName, properties: new PropertyDescription[]{},
+                functions: functions);
+        }
+
+        private string buildTemporaryStub(string namespaceName, string className)
+        {
+            var description = new ClassDescription(className, namespaceName, new PropertyDescription[]{}, new FunctionDescription[] { });
+            var generator = new ClassGenerator(description);
+            return generator.GenerateClass();
         }
     }
 }
---
+++ Speccer/Analysis/FunctionLookup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Speccer.Description;
+
+namespace Speccer.Analysis
+{
+    public static class FunctionLookup
+    {
+        public static List<FunctionDescription> GetFunctionCallsOn(this SyntaxTree tree, Compilation compilation,
+            string className)
+        {
+            return new FunctionDescription[] { }.ToList();
+        }
+    }
+}
